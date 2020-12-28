4a6a1f69235d63eca3e8d6ca053ed321b59a0edd
Gam <trant48@student.douglascollege.ca>
2020-12-28 12:30:42 -0800

    create app search wheather by city

--- app.js
+++ app.js
@@ -1,22 +1,44 @@
 const express = require("express");
 const https = require("https");
+const bodyParser = require("body-parser");
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
 app.get("/", function(req, res) {
-    const url = "https://api.openweathermap.org/data/2.5/weather?q=London&appid=f0f75dc637814a8ba04169aaf1614336";
+    res.sendFile(__dirname + "/index.html");
+
+
+});
+
+app.post("/", function(req, res) {
+    console.log(req.body.cityName);
+
+    const query = req.body.cityName;
+    const apiKey = "f0f75dc637814a8ba04169aaf1614336";
+    const unit = "metric";
+    const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&appid=" + apiKey + "&units=" + unit + "";
     https.get(url, function(response) {
         console.log(response.statusCode);
         response.on("data", function(data) {
             const weatherData = JSON.parse(data);
             const temp = weatherData.main.temp;
-            console.log(temp);
-        })
+            const weatherDescription = weatherData.weather[0].description
+            const weatherIcon = weatherData.weather[0].icon;
+            const imageURL = "http://openweathermap.org/img/wn/" + weatherIcon + "@2x.png";
+
+            res.write("<p>The weather description is " + weatherDescription + " </p>");
+            res.write("<h1>The temperature in " + query + " is " + temp + " degree Celcius.</h1>");
+            res.write("<img src=" + imageURL + ">");
+            res.send();
+
+
+        });
     });
-    res.send("Server is up and running.");
+
 });
-
-
 
 app.listen(3000, function() {
     console.log("Server is running on port 3000.");
-})
+});
